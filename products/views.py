3f7c056c84ef08211cd4b848eb6f4ecec05c5223
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2020-10-06 11:00:46 +0900

    Modified: Modified Endpoint - Delete the FK in DeliveryType table (unused due to MTM field) - Modified the spelling of field (delievery -> delivery) - Add MTM Table (ProductTag) - Add SQL File in .gitignore - Add Category Endpoint - Add SubCategory Endpoint - Add ProductsList Endpoint - Add ProductDetail Endpoint - Add ProductSearch Endpoint - Modified: ProductsList Endpoint (to make filters) - Modified: ProdicutDetail Endpoint - Add Elasticsearch Index - Add Elasticsearch Query - Add Seach Endpoint - Modified requirements.txt - Modified product list filter (sub_category_id, best -> order-by review)

--- products/views.py
+++ products/views.py
@@ -1,16 +1,17 @@
 import json
 import redis
-import time
+import random
 
 from django.views      import View
 from django.http       import JsonResponse
 from django.core.cache import cache
-from django.db.models  import Q, F, Value, Case, When, IntegerField, JSONField
+from django.db.models  import F, Count, Value, IntegerField, JSONField
 
 from elasticsearch     import Elasticsearch, helpers
 
 from .models           import Category, SubCategory, Product, ProductDeliveryType, ProductTag, ProductSeries
-from .convert          import korean_to_englished_string
+from .documents        import get_instance, get_query
+from review.models     import Review
 
 
 class CategoryView(View):
@@ -33,21 +34,79 @@ class SubCategoryView(View):
 class ProductsListView(View):
     def post(self, request):
         data = json.loads(request.body)
+        category_id = data.get('category_id', None)
         sub_category_id = data.get('sub_category_id', None)
         sort_by_category = data.get('sort_by_category', '컬리추천')
+        sort_by_sub_category = data.get('sort_by_sub_category', None)
         sort_by_delivery = data.get('sort_by_delivery', '샛별지역상품')
-        sort_by_filter = data.get('sort_by_filter', '신상품순')
+        sort_by_filter = data.get('sort_by_filter', None)
 
-        # products = cache.get('products')
+        products = cache.get('products')
 
-        products = Product.objects.prefetch_related('discout', 'tag', 'delivery_type').values(
-            'id', 'name', 'image', 'discount__image', 'price', 'discount__percentage',
-            'tag__name', 'delivery_type__name', 'is_recommended', 'is_new'
-        ).annotate(
-            tag_names = Value([], output_field = JSONField()),
-            delivery_types = Value([], output_field = JSONField()),
-            discount_price = Value(1, output_field = IntegerField())
-        )
+        if not products:
+            products = Product.objects.prefetch_related('discount', 'tag', 'delivery_type', 'review_set').values(
+                'id', 'name', 'image', 'discount__image', 'price', 'discount__percentage', 'is_recommended', 'is_new'
+            ).annotate(
+                tag_names = Value([], output_field = JSONField()),
+                delivery_types = Value([], output_field = JSONField()),
+                discount_price = Value(1, output_field = IntegerField())
+            )
+        
+        cache.set('products', products)
+
+        if sub_category_id:
+            sub_category_name = SubCategory.objects.get(id = sub_category_id)
+            if sub_category_name == '전체보기':
+                category_id = Category.objects.get(subcategory__id = sub_category_id).id
+                products = products.filter(sub_category_category__id = category_id)
+            else:    
+                products = products.filter(sub_category__id = sub_category_id)
+
+        if sort_by_sub_category == 'MD의 추천':
+            products = products.filter(is_recommended = True, sub_category__category_id = category_id)
+
+        if sort_by_sub_category == "알뜰 상품":
+            products = products.exclude(discount__percentage = None)
+
+        if sort_by_sub_category == 'MD의 추천':
+            products = products.filter(is_recommended = True)
+
+        if sort_by_sub_category == "오늘의 신상품":
+            products = products.filter(is_new = True)
+
+        if sort_by_sub_category == "지금 가장 핫한 상품":
+            products = products.exclude(review__id = None)
+
+        if sort_by_category == '신상품':
+            sort_by_filter == "신상품순"
+
+        if sort_by_category == "알뜰쇼핑":
+            products = products.exclude(discount__percentage = None)
+
+        if sort_by_category == "베스트":
+            products = products.exclude(review__id = None)
+
+        if sort_by_category != "컬리추천":
+            if sort_by_filter == "혜택순":
+                products = products.all().order_by('-discount__percentage')
+
+            if sort_by_filter == "신상품순":
+                products = products.filter(is_new = True)
+        
+            if sort_by_filter == "인기상품순":
+                products = products.all().annotate(count = Count('review__product_id')).order_by('-count')
+
+            if sort_by_filter == "낮은 가격순":
+                products = products.all().order_by('price')
+            
+            if sort_by_filter == "높은 가격순":
+                products = products.all().order_by('-price')
+
+            if sort_by_filter == "추천순":
+                products = products.order_by('-is_recommended')
+        
+            products = products.filter(delivery_type__name__contains = sort_by_delivery[:2])
+
         for product in products:
             tag_names = [
                 tags.tag.name for tags in ProductTag.objects.filter(product_id = product['id']) if tags
@@ -59,49 +118,13 @@ class ProductsListView(View):
                 discount_percentage = 1 - (product['discount__percentage'] / 100)
             else:
                 discount_percentage = 1
+
             product['tag_names'] = tag_names
             product['delivery_types'] = delivery_types
-            product['discount_price'] = product['price'] * discount_percentage
+            product['discount_price'] = int(product['price'] * discount_percentage)
 
-        if sub_category_id:
-            products = products.filter(sub_category__id = sub_category_id)
-
-        # cache.set('products', products)
-
-        # if sub_category_id
-
-        if sort_by_category == '컬리추천':
-            products = products.filter(is_recommended = True)
-            
-        if sort_by_category == '신상품':
-            products = products.filter(is_new = True)
-            
-        
-        # if sort_by_category == '베스트':
-
-
-        # if sort_by_category == '알뜰쇼핑':
-        #     sort_by_filter = '혜택순'
-        
-        # if sort_by_category == '추천':
-
-
-        # if sort == '추천순' or sort == 'MD의 추천':
-
-        # if sort == '신상품순' or sort == '신상품':
-        
-        # if sort == '인기상품순':
-
-        # if sort == '낮은 가격순':
-
-        # if sort == '높은 가격순':
-
-        # if sort == '알뜰 상품' or '알뜰쇼핑'
-
-        # if sort_by_delivery == '샛별지역상품':
-        #     products = products.filter('devliery_types__contains' = sort_by_delivery)
-        
-        # if sort_by_delivery == '택배지역상품':
+        if sort_by_sub_category == "이 상품 어때요?":
+            products = random.choices(products, k=8)
 
         return JsonResponse(
             {'products': list(products)},
@@ -133,10 +156,10 @@ class ProductDetailView(View):
         else:
             discount_percentage = 1
 
-        product['discount_price'] = product['price'] * discount_percentage
+        product['discount_price'] = int(product['price'] * discount_percentage)
         
         for series_price in product['product_series']:
-            series_price['price'] = series_price['price'] * discount_percentage
+            series_price['price'] = int(series_price['price'] * discount_percentage)
 
         return JsonResponse(
             {'product': product},
@@ -145,77 +168,21 @@ class ProductDetailView(View):
 
 
 class ProductSearchView(View):
-    def get(self, request):
-        es = Elasticsearch()
-        index = 'products'
-        body = {
-            "settings": {
-                "index": {
-                    "number_of_shards": 1,
-                    "number_of_replicas": 0,
-                    "max_ngram_diff": 55
-                },
-                "analysis": {
-                    "analyzer": {
-                        "name_analyzer": {
-                            "type": "custom",
-                            "tokenizer": "ngram_tokenizer"
-                        }
-                    },
-                    "tokenizer": {
-                        "ngram_tokenizer": {
-                            "type": "ngram",
-                            "min_gram": 1,
-                            "max_gram": 50,
-                            "token_chars": [
-                                "letter"
-                            ]
-                        }
-                    }
-                }
-            },
-            "mappings": {
-                "properties": {
-                    "name": {
-                        "type": "text",
-                        "analyzer": "name_analyzer",
-                        "search_analyzer": "name_analyzer"
-                    },
-                    "converted_name": {
-                        "type": "text",
-                        "analyzer": "name_analyzer",
-                        "search_analyzer": "name_analyzer",
-                    }
-                }
-            }
-        }
-        es.indices.delete(index = index)
-        print(es.indices.create(index = index, body = body))
+    def post(self, request):
+        data = json.loads(request.body)
+        keyword = data['keyword']
 
-        products = Product.objects.values('name')
-        actions = [
-            {
-                "_index": index,
-                "_id": idx + 1,
-                "_source": {
-                    "name": product['name'],
-                    "converted_name": korean_to_englished_string(product['name'])
-                }
-            }
-            for idx, product in enumerate(products)
-        ]
+        es = get_instance()
+        results = es.search(index='products', body=get_query(keyword))
 
-        helpers.bulk(es, actions=actions, index=index)
-
-        time.sleep(3)
-
-        results = es.search(index=index, body={'query': {'match_all': {}}, 'size': 300})
-
-        res = []
+        products = []
         for result in results['hits']['hits']:
-            res.append(result['_source']['name'])
-
+            product = {
+                'id': result['_id'],
+                'name': result['_source']['name']
+            }
+            products.append(product)
         return JsonResponse(
-            {'products': res},
+            {'products': products},
             status = 200
         )
