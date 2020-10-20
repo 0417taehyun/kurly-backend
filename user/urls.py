208a6c25d9e0a79c459f4408a8f28b94bdef7281
KimHyunWoo <miretta20@gmail.com>
2020-10-20 16:13:17 +0900

    MODIFY: feature/review 수정완료

--- user/urls.py
+++ user/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     SignUpView,
     SignInView,
-    GoogleSignInView
+    GoogleSignInView,
+    CartView
 )
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
     path('/googlesignin', GoogleSignInView.as_view()),
+    path('/cart', CartView.as_view())
 ]
